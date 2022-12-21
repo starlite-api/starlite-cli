e0b0867eab635c94cdd4dd6ef470f23d329bd202
provinzkraut <25355197+provinzkraut@users.noreply.github.com>
2022-12-21 19:40:21 +0100

    decouple from starlite

--- starlite_cli/main.py
+++ starlite_cli/main.py
@@ -5,30 +5,18 @@ import inspect
 from functools import wraps
 from os import getenv
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Callable, Iterable, TypeVar
+from typing import TYPE_CHECKING, Any, Callable, Iterable, TypeVar, cast
 
 import rich
 import rich.tree
-from click import (
-    ClickException,
-    Command,
-    Group,
-    argument,
-    confirm,
-    group,
-    option,
-    style,
-)
+from click import ClickException, Command, Group, argument, group, option, style
 from rich.console import Console
+from rich.prompt import Confirm
 from rich.table import Table
 from typing_extensions import ParamSpec
 
-from starlite import DefineMiddleware, HTTPRoute, Starlite, WebSocketRoute
-from starlite.utils import is_class_and_subclass
-from starlite.utils.cli import CLI_INIT_CALLBACKS
-from starlite.utils.helpers import get_name, unwrap_partial
-
 if TYPE_CHECKING:
+    from starlite import Starlite  # noqa: TC004
     from starlite.middleware.session.base import ServerSideBackend
 
 
@@ -43,12 +31,18 @@ class _StarliteCLIException(ClickException):
         super().__init__(style(message, fg="red"))
 
 
+def _load_app_from_path(app_path: str) -> Starlite:
+    module_path, app_name = app_path.split(":")
+    module = importlib.import_module(module_path)
+    return cast(Starlite, getattr(module, app_name))
+
+
 def _autodiscover_app() -> tuple[str, Starlite]:
+    from starlite import Starlite
+
     if app_path := getenv("STARLITE_APP"):
         console.print(f"Using starlite app from env: [bright_blue]{app_path!r}")
-        module_path, app_name = app_path.split(":")
-        module = importlib.import_module(module_path)
-        return app_path, getattr(module, app_name)
+        return app_path, _load_app_from_path(app_path)
 
     cwd = Path().cwd()
     for name in ["asgi.py", "app.py", "application.py"]:
@@ -66,10 +60,6 @@ def _autodiscover_app() -> tuple[str, Starlite]:
     raise _StarliteCLIException("Could not find a Starlite app")
 
 
-def _get_app() -> Starlite:
-    return _autodiscover_app()[1]
-
-
 def _inject_app(func: Callable[P, T]) -> Callable[P, T]:
     """Inject the app instance into a `Command`"""
     if "app" not in inspect.signature(func).parameters:
@@ -77,7 +67,7 @@ def _inject_app(func: Callable[P, T]) -> Callable[P, T]:
 
     @wraps(func)
     def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
-        kwargs["app"] = _get_app()
+        kwargs["app"] = _autodiscover_app()[1]
         return func(*args, **kwargs)
 
     return wrapped
@@ -91,27 +81,35 @@ def _wrap_commands(commands: Iterable[Command]) -> None:
             command.callback = _inject_app(command.callback)
 
 
-def _bool_enabled(value: Any) -> str:
+def _format_is_enabled(value: Any) -> str:
+    """Return a coloured string `"Enabled" if `value` is truthy, else "Disabled"."""
     if value:
         return "[green]Enabled[/]"
     return "[red]Disabled[/]"
 
 
 def _show_app_info(app: Starlite) -> None:
+    """Display basic information about the application and its configuration."""
+    from starlite import DefineMiddleware
+    from starlite.utils import get_name
+
     table = Table(show_header=False)
     table.add_column("title", style="cyan")
     table.add_column("value", style="bright_blue")
 
-    table.add_row("Debug mode", _bool_enabled(app.debug))
-    table.add_row("CORS", _bool_enabled(app.cors_config))
-    table.add_row("CSRF", _bool_enabled(app.csrf_config))
+    table.add_row("Debug mode", _format_is_enabled(app.debug))
+    table.add_row("CORS", _format_is_enabled(app.cors_config))
+    table.add_row("CSRF", _format_is_enabled(app.csrf_config))
     if app.allowed_hosts:
         allowed_hosts = app.allowed_hosts
 
         table.add_row("Allowed hosts", ", ".join(allowed_hosts.allowed_hosts))
 
-    table.add_row("Request caching", _bool_enabled(app.cache))
-    table.add_row("OpenAPI", _bool_enabled(app.openapi_config))
+    table.add_row("Request caching", _format_is_enabled(app.cache))
+    openapi_enabled = _format_is_enabled(app.openapi_config)
+    if app.openapi_config:
+        openapi_enabled += f" path=[yellow]{app.openapi_config.openapi_controller.path}"
+    table.add_row("OpenAPI", openapi_enabled)
 
     table.add_row("Compression", app.compression_config.backend if app.compression_config else "[red]Disabled")
 
@@ -126,7 +124,7 @@ def _show_app_info(app: Starlite) -> None:
         for static_files in static_files_configs:
             static_files_info.append(
                 f"path=[yellow]{static_files.path}[/] dirs=[yellow]{', '.join(map(str, static_files.directories))}[/] "
-                f"html_mode={_bool_enabled(static_files.html_mode)}",
+                f"html_mode={_format_is_enabled(static_files.html_mode)}",
             )
         table.add_row("Static files", "\n".join(static_files_info))
 
@@ -148,33 +146,48 @@ def _show_app_info(app: Starlite) -> None:
 @group()
 def cli() -> None:
     """Starlite CLI."""
-    for callback in CLI_INIT_CALLBACKS:
-        callback(cli)
+    try:
+        # we only want to run this if starlite is installed in the current environment
+        # since we could also be running a bootstrapping command, which does not require
+        # starlite to be installed
+        from starlite.utils.cli import CLI_INIT_CALLBACKS
 
-    _wrap_commands(cli.commands.values())
+        for callback in CLI_INIT_CALLBACKS:
+            callback(cli)
+        _wrap_commands(cli.commands.values())
+
+    except ImportError:
+        pass
 
 
 @cli.command(name="info")
-def info_command() -> None:
+def info_command(app: Starlite) -> None:
     """Show information about the detected Starlite app."""
-    _show_app_info(_get_app())
+    _show_app_info(app)
 
 
 @cli.command()
 @option("-r", "--reload", help="Reload server on changes", default=False)
 @option("-p", "--port", help="Serve under this port", type=int, default=8000)
 @option("--host", help="Server under this host", default="127.0.0.1")
-def run(reload: bool, port: int, host: str) -> None:
+@option("--app", "app_path", help="Starlite app to run")
+def run(reload: bool, port: int, host: str, app_path: str | None) -> None:
     """Run a Starlite app.
 
-    The app can be specified in the STARLITE_APP environment variable, or an asgi.py, app.py or application.py file.
+    The app can be either passed as a module path in the form of <module name>.<submodule>:<app instance>, or set as an
+    environment variable STARLITE_APP with the same format. If none of those is given, the app will be automatically
+    discovered from a file with one of the canonical names: app.py, asgi.py or application.py
     """
     try:
         import uvicorn
     except ImportError:
         raise _StarliteCLIException("Uvicorn needs to be installed to run an app")  # pylint: disable=W0707
 
-    app_path, app = _autodiscover_app()
+    if not app_path:
+        app_path, app = _autodiscover_app()
+    else:
+        app = _load_app_from_path(app_path)
+
     _show_app_info(app)
 
     console.rule("[yellow]Starting server process", align="left")
@@ -185,6 +198,9 @@ def run(reload: bool, port: int, host: str) -> None:
 @cli.command()
 def routes(app: Starlite) -> None:
     """Display information about the application's routes."""
+    from starlite import HTTPRoute, WebSocketRoute
+    from starlite.utils.helpers import unwrap_partial
+
     tree = rich.tree.Tree("", hide_root=True)
 
     for route in sorted(app.routes, key=lambda r: r.path):
@@ -220,7 +236,9 @@ def routes(app: Starlite) -> None:
 
 
 def _get_session_backend(app: Starlite) -> ServerSideBackend:
+    from starlite import DefineMiddleware
     from starlite.middleware.session.base import ServerSideBackend, SessionMiddleware
+    from starlite.utils import is_class_and_subclass
 
     for middleware in app.middleware:
         if isinstance(middleware, DefineMiddleware):
@@ -249,7 +267,7 @@ def delete_session(session_id: str, app: Starlite) -> None:
         console.print(f"[red]Session {session_id!r} not found")
         return
 
-    if confirm(style(f"Delete session {session_id!r}?", fg="red"), abort=True):
+    if Confirm.ask(f"Delete session {session_id!r}?"):
         anyio.run(backend.delete, session_id)
         console.print(f"[green]Deleted session {session_id!r}")
 
@@ -261,6 +279,6 @@ def clear_sessions(app: Starlite) -> None:
 
     backend = _get_session_backend(app)
 
-    if confirm(style("Delete all active sessions?", fg="red"), abort=True):
+    if Confirm.ask("[red]Delete all active sessions?"):
         anyio.run(backend.delete_all)
         console.print("[green]All active sessions deleted")
